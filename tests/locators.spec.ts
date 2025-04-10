9ca5a8f04b7e6a5b81e3dc1ed8b7c19c2a3fa374
Vitaliy Melnyk <vitaliy.melnyk@techmagic.co>
2025-04-10 17:11:04 +0300

    user facing locators

---
+++ tests/locators.spec.ts
@@ -0,0 +1,61 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach( async ({page}) => {
+    await page.goto('https://www.w3schools.com');
+});
+
+test('getByText', async ({page}) => {
+    await page.getByText(`Not Sure Where To Begin?`).click();
+
+    await expect(page.getByText(`To become a web developer`)).toBeVisible();
+});
+
+test('getByPlaceholder', async ({page}) => {
+    const serachField = page.getByPlaceholder('Search...');
+    await serachField.fill('typescript');
+});
+
+test('getByLabel', async ({page}) => {
+    const serachField = page.getByLabel('Search field', {exact: true});
+    await serachField.fill('typescript');
+});
+
+test('getByTitle', async ({page}) => {
+    await page.getByText(`Not Sure Where To Begin?`).click();
+
+    await page.waitForTimeout(1000);
+
+    await page.getByTitle('Home').click();
+});
+
+test('getByAltText', async ({page}) => {
+    const colorPicker = page.getByAltText('Colorpicker');
+
+    await colorPicker.scrollIntoViewIfNeeded();
+
+    await colorPicker.click();
+});
+
+test('getByRole', async ({page}) => {
+    const serachField = page.getByRole('textbox', {name: 'Search field'});
+
+    await serachField.fill('typescript');
+});
+
+test('getByRole 2', async ({page}) => {
+    const forTeachersButton = page.getByRole('link', {name: 'W3Schools Academy'});
+
+    await forTeachersButton.click();
+});
+
+test('getByRole 3', async ({page}) => {
+    const themeToggle = page.getByRole('button', {name: 'Toggle'});
+
+    await themeToggle.click();
+});
+
+
+test.afterEach( async ({page}) => {
+    await page.waitForTimeout(3000);
+    page.close();
+});
